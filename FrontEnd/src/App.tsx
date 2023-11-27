df8375d8a8956ee27561c4e25d795e0ebb686410
Rocio Alcala <rro.alcala@gmail.com>
2023-11-27 15:50:45 -0300

    chore: adding header

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 const App: React.FC = () => {
   return (
     <>
-      <h1 className='text-6xl text-center font-bold text-blue-800'>Biblioteca web</h1>
+      <header>
+        <nav></nav>
+      </header>
+      <h1 className="text-6xl text-center font-bold text-blue-800">
+        Biblioteca web
+      </h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
