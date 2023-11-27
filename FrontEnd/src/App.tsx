4cffc40171b1215aa12792131e86c20c78384d22
Lohana <Loha@hotmail.com>
2023-11-27 15:55:37 +0100

    Inicio proyecto react con dependencias

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const App: React.FC = () => {
+  return (
+    <>
+      <h1 className='text-6xl text-center font-bold text-blue-800'>Biblioteca web</h1>
+    </>
+  )
+}
+
+export default App
