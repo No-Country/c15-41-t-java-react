de12e092e548ea1acfce622b6986ade21e817309
jhordyess <dev@jhordyess.com>
2023-11-27 19:31:37 +0000

    Añadido prettier y plugin para TailwindCSS - Añadido un nuevo comando con NPM - Formateado la carpeta src - Añadido el archivo de configuración de prettier

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -5,12 +5,10 @@ const App: React.FC = () => {
       <header>
         <nav></nav>
       </header>
-      <h1 className="text-6xl text-center font-bold text-blue-800">
-        Biblioteca web
-      </h1>
+      <h1 className="text-center text-6xl font-bold text-blue-800">Biblioteca web</h1>
       <footer>2023</footer>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
