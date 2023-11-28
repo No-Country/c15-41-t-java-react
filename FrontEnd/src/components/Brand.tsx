91f682b95d0fe0bf3846ab5c937b71887efe0677
Matias Luengo <matiasluengoo95@gmail.com>
2023-11-28 16:48:07 -0300

    Adding Brand component and isotipo

---
+++ FrontEnd/src/components/Brand.tsx
@@ -0,0 +1,5 @@
+import isologotipo from '../../public/isologotipo.png'
+
+export const Brand: React.FC = () => {
+  return <img className=" max-w-[15rem]" src={isologotipo} alt="Isologotipo de Bibliotech" />
+}
