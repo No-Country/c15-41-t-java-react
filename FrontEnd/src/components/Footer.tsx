24c8f08b1f4562e10d9fcde378a5e4dae2fb4217
jhordyess <dev@jhordyess.com>
2023-11-29 23:33:34 +0000

    Actualizado footer

--- FrontEnd/src/components/Footer.tsx
+++ FrontEnd/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="flex h-16 w-full items-center justify-center bg-blueDark p-5 max-md:hidden">
+    <div className="flex h-16 w-full items-center justify-center bg-blueDark p-5 max-lg:hidden">
       <p className="text-xs font-bold text-white">Copyright 2023 (c).Grupo C15-41-t-java-react</p>
     </div>
   )
--- FrontEnd/src/components/NavBarMobile.tsx
+++ FrontEnd/src/components/NavBarMobile.tsx
@@ -6,7 +6,7 @@ import imgOpenBook from '../assets/icons-mobile/OpenBook.png'
 
 export const NavBarMobile: React.FC = () => {
   return (
-    <nav className=" grid h-fit w-full grid-cols-4 items-center bg-[#000842] py-10">
+    <nav className="hidden h-fit w-full grid-cols-4 items-center bg-[#000842] py-10 max-lg:grid">
       <Link
         to="/home"
         className="text-md mx-auto flex w-12 flex-col items-center gap-y-2 text-center font-bold text-white no-underline hover:underline"
