9daff2a2cc47b47459948f3cb984eae9c1346cdb
jhordyess <dev@jhordyess.com>
2023-11-29 16:07:07 +0000

    Configurado y ordenado las rutas

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { Login } from './routes/Login.tsx'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Layout from './components/Layout.tsx'
+import Home from './components/Home.tsx'
+import { useUser } from './context/UserContext.tsx'
 
-const App: React.FC = () => {
+const App = () => {
+  const { currentUser, setupComplete } = useUser()
+
+  if (setupComplete) {
+    return <div>Loading...</div>
+  }
+
+  const PrivateLayout = () => (currentUser.isLoggedIn ? <Layout /> : <Navigate to="/" />)
+
+  const RedirectLogin = () => (currentUser.isLoggedIn ? <Navigate to="/home" /> : <Login />)
+
   return (
-    <>
-      <h1 className="mt-2 text-center text-6xl font-bold text-blue-800">Biblioteca web</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RedirectLogin />} />
+        <Route element={<PrivateLayout />}>
+          <Route path="/home" element={<Home />} />
+          <Route path="/libros" element={<p>Libros page</p>} />
+          <Route path="/prestamos" element={<p>Prestamos page</p>} />
+          <Route path="/usuarios" element={<p>Usuarios</p>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Login } from './routes/Login.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout.tsx'
-import Home from './components/Home.tsx'
+import { UserProvider } from './context/UserContext.tsx'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route element={<Layout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/libros" element={<p>Libros page</p>} />
-          <Route path="/prestamos" element={<p>Prestamos page</p>} />
-          <Route path="/usuarios" element={<p>Usuarios</p>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 )
