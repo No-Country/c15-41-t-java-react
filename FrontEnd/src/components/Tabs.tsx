73d855c7327bf15fd2a883a230e34e634d0a3614
jhordyess <dev@jhordyess.com>
2023-11-29 19:55:56 +0000

    Configurado estilos de Tabs

--- FrontEnd/src/components/Tabs.tsx
+++ FrontEnd/src/components/Tabs.tsx
@@ -1,20 +1,30 @@
 import { Tab, Tabs as ReactTabs, TabList, TabPanel } from 'react-tabs'
-import 'react-tabs/style/react-tabs.css'
+
 const Tabs = () => {
   return (
-    <div>
-      <ReactTabs>
-        <TabList>
-          <Tab selectedClassName="bg-blueLight">Catálogo Completo</Tab>
-          <Tab>Agregar Libro</Tab>
-          <Tab>Búsqueda</Tab>
-        </TabList>
+    <ReactTabs className="w-full px-24">
+      <TabList className="flex h-[44px] w-full list-none justify-evenly gap-x-1">
+        <Tab
+          className="flex w-full items-center justify-center bg-[#E6E7E9] text-base font-[500] leading-[24px] text-blueLight hover:cursor-pointer aria-selected:bg-blueLight aria-selected:text-white"
+          selectedClassName="bg-blueLight"
+        >
+          Ver catálogo completo
+        </Tab>
+        <Tab className="flex w-full items-center justify-center bg-[#E6E7E9] text-base font-[500] leading-[24px] text-blueLight hover:cursor-pointer aria-selected:bg-blueLight aria-selected:text-white">
+          Agregar Libro
+        </Tab>
+        <Tab
+          className="flex w-full items-center justify-center bg-[#E6E7E9] text-base font-[500] leading-[24px] text-blueLight hover:cursor-pointer aria-selected:bg-blueLight aria-selected:text-white"
+          selectedClassName="bg-blueLight"
+        >
+          Buscador
+        </Tab>
+      </TabList>
 
-        <TabPanel>{}</TabPanel>
-        <TabPanel>{'Agregar Libro'}</TabPanel>
-        <TabPanel>{'Busqueda'}</TabPanel>
-      </ReactTabs>
-    </div>
+      <TabPanel>{}</TabPanel>
+      <TabPanel>{'Agregar Libro'}</TabPanel>
+      <TabPanel>{'Busqueda'}</TabPanel>
+    </ReactTabs>
   )
 }
 
