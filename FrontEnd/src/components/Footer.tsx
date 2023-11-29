510ddc1b9f9f21ab767afb9206ef39fbc7cc700d
jhordyess <dev@jhordyess.com>
2023-11-29 23:17:04 +0000

    Revertido cambios en Footer

--- FrontEnd/src/components/Footer.tsx
+++ FrontEnd/src/components/Footer.tsx
@@ -1,10 +1,7 @@
 const Footer = () => {
   return (
-    <div className="flex h-16 w-full items-center justify-center bg-blueDark p-5 max-lg:h-[104px]">
-      <p className="hidden max-md:block">Componente de nav Aquí</p>
-      <p className="text-xs font-bold text-white max-md:hidden">
-        Copyright 2023 (c).Grupo C15-41-t-java-react
-      </p>
+    <div className="flex h-16 w-full items-center justify-center bg-blueDark p-5 max-md:hidden">
+      <p className="text-xs font-bold text-white">Copyright 2023 (c).Grupo C15-41-t-java-react</p>
     </div>
   )
 }
