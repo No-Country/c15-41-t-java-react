3dfc696883a20f3eb199a4776e9de0f737f261be
Lohana Orellano <lohanaorellano_lsi@hotmail.comgit>
2023-12-01 20:10:05 +0100

    modify bookList

--- FrontEnd/src/components/Tabs.tsx
+++ FrontEnd/src/components/Tabs.tsx
@@ -1,12 +1,11 @@
 import { Tab, Tabs as ReactTabs, TabList, TabPanel } from 'react-tabs'
 import 'react-tabs/style/react-tabs.css'
 import RegisterForm from './RegisterForm'
-import { SearchBook } from './SearchBook'
 import BookList from './BookList'
 
 const Tabs = () => {
   return (
-    <ReactTabs className="w-full px-24 max-lg:px-12 max-sm:px-6">
+    <ReactTabs className="w-full px-24 max-lg:paddx-12 max-sm:px-6">
       <TabList className="flex h-11 w-full list-none justify-evenly gap-x-1 max-lg:h-14">
         <Tab
           className="flex w-full flex-col flex-wrap items-center justify-center bg-[#E6E7E9] px-2 text-center text-base font-[500] leading-[24px] text-blueLight hover:cursor-pointer aria-selected:bg-blueLight aria-selected:text-white"
