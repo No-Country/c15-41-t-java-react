e8ce41bb11413891d0cc706e61f8b747c45b4bfa
gmanselmo <130943341+gmanselmo@users.noreply.github.com>
2023-12-04 17:04:04 -0300

    Update UserRepository.java

--- LibraryBackEnd/src/main/java/c1541tjavareact/library/domain/repository/UserRepository.java
+++ LibraryBackEnd/src/main/java/c1541tjavareact/library/domain/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     public List<User> searchByName(String searchParam);
 
     public List<User> searchByLastname(String searchParam);
