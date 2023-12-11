2d030fecbb40030421a38d89e4b218e6c47c853d
jhordyess <dev@jhordyess.com>
2023-12-11 18:52:32 +0000

    Añadido redirección a lista luego de añadir

--- FrontEnd/src/pages/Catalogue.tsx
+++ FrontEnd/src/pages/Catalogue.tsx
@@ -2,10 +2,21 @@ import { Tab, Tabs as ReactTabs, TabList, TabPanel } from 'react-tabs'
 import 'react-tabs/style/react-tabs.css'
 import RegisterForm from '../components/RegisterForm'
 import BookList from '../components/BookList'
+import { useState } from 'react'
 
 const Tabs = () => {
+  const [tabIndex, setTabIndex] = useState(0)
+
+  const reopenList = () => {
+    setTabIndex(0)
+  }
+
   return (
-    <ReactTabs className="w-full px-24 max-lg:px-12 max-sm:px-6">
+    <ReactTabs
+      className="w-full px-24 max-lg:px-12 max-sm:px-6"
+      selectedIndex={tabIndex}
+      onSelect={index => setTabIndex(index)}
+    >
       <TabList className="flex h-11 w-full list-none justify-evenly gap-x-1 max-lg:h-14">
         <Tab
           className="flex w-full flex-col flex-wrap items-center justify-center bg-[#E6E7E9] px-2 text-center text-base font-[500] leading-[24px] text-blueLight hover:cursor-pointer aria-selected:bg-blueLight aria-selected:text-white"
@@ -22,7 +33,7 @@ const Tabs = () => {
         <BookList />
       </TabPanel>
       <TabPanel>
-        <RegisterForm />
+        <RegisterForm onSuccess={reopenList} />
       </TabPanel>
     </ReactTabs>
   )
