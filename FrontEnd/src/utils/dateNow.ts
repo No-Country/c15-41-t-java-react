2801a5a58015946e843b46cbec9fe96327b3510c
Lohana Orellano <lohanaorellano_lsi@hotmail.comgit>
2023-12-13 16:42:32 +0100

    add component BookDetail and LoanRegister

---
+++ FrontEnd/src/utils/dateNow.ts
@@ -0,0 +1,11 @@
+export function getFormattedTodayDate() {
+    const today = new Date();
+  
+    const day = today.getDate().toString().padStart(2, '0');
+    const month = (today.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
+    const year = today.getFullYear();
+  
+    const formattedDate = `${day}/${month}/${year}`;
+  
+    return formattedDate;
+  }
