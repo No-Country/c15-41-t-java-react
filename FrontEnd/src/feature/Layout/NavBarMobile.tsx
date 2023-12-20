bbd9d56d8669582e653ad835dc9266fd13135455
Lohana Orellano <lohanaorellano_lsi@hotmail.comgit>
2023-12-20 18:06:08 +0100

    styled NavBarMobile

--- FrontEnd/src/feature/Layout/NavBarMobile.tsx
+++ FrontEnd/src/feature/Layout/NavBarMobile.tsx
@@ -2,31 +2,31 @@ import { Link } from 'react-router-dom'
 
 export const NavBarMobile: React.FC = () => {
   return (
-    <nav className="fixed bottom-0 z-50 hidden h-fit w-full grid-cols-4 items-center bg-[#000842] py-10 max-lg:grid">
+    <nav className="fixed bottom-0 z-50 hidden h-fit w-full grid-cols-4 items-center bg-[#000842] py-4 max-lg:grid">
       <Link
         to="/home"
-        className="text-md mx-auto flex w-12 flex-col items-center gap-y-2 text-center font-bold text-white no-underline hover:underline"
+        className="text-sm mx-auto flex w-12 flex-col items-center gap-y-2 text-center font-bold text-white no-underline hover:underline"
       >
         <img className="w-full" src="/icons/mobile/Home.png" alt="Icono de home" />
         Inicio
       </Link>
       <Link
         to="/libros"
-        className="text-md mx-auto flex w-12 flex-col items-center gap-y-2 text-center font-bold text-white no-underline hover:underline"
+        className="text-sm mx-auto flex w-12 flex-col items-center gap-y-2 text-center font-bold text-white no-underline hover:underline"
       >
         <img className="w-full" src="/icons/mobile/OpenBook.png" alt="Icono de libro abierto" />
         Libros
       </Link>
       <Link
         to="/prestamos"
-        className="text-md mx-auto flex w-12 flex-col items-center gap-y-2 text-center font-bold text-white no-underline hover:underline"
+        className="text-sm mx-auto flex w-12 flex-col items-center gap-y-2 text-center font-bold text-white no-underline hover:underline"
       >
         <img className="w-full" src="/icons/mobile/Book.png" alt="Icono de libro" />
         Préstamos
       </Link>
       <Link
         to="/miembros"
-        className="text-md mx-auto flex w-12 flex-col items-center gap-y-2 text-center font-bold text-white no-underline hover:underline"
+        className="text-sm mx-auto flex w-12 flex-col items-center gap-y-2 text-center font-bold text-white no-underline hover:underline"
       >
         <img className="w-full" src="/icons/mobile/Customer.png" alt="Icono de miembro" />
         Miembros
